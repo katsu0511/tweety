3c39d3c65f0300540753ba46ce691ab6c3460a84
Katsuya <haradakatsuya190511@gmail.com>
2020-09-25 22:03:18 +0900

    created profile show

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,16 @@ class User < ApplicationRecord
   validates :account_name, uniqueness: true
 
   has_one :profile, dependent: :destroy
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default.png'
+    end
+  end
 end
