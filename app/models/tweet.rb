4f054a7307a979fd24d50d0ce75d0f27304c9098
Katsuya <haradakatsuya190511@gmail.com>
2020-09-27 12:49:54 +0900

    created tweet model

---
+++ app/models/tweet.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_tweets_on_user_id  (user_id)
+#
+class Tweet < ApplicationRecord
+  belongs_to :user
+end
