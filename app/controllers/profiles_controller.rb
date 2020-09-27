77d44b70c9562fdca579c0b7346908f09751ddf6
Katsuya <haradakatsuya190511@gmail.com>
2020-09-27 12:38:12 +0900

    set in order to enable to update profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,5 +9,23 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを更新しました'
+    else
+      flash.now[:error] = '更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :name,
+      :introduction,
+      :birthday,
+      :icon
+    )
   end
 end
