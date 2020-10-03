d7e233e5b2c1beac3514dfb47be37e7e7d284a49
Katsuya <haradakatsuya190511@gmail.com>
2020-10-03 16:55:09 +0900

    created account show routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
+  resources :accounts, only: [:show]
+
   resource :timeline, only: [:show]
   resource :profile, only: [:show, :edit, :update]
 
